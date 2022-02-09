f7e9cffa44af6a8349d155d71ede275339ad9dec
GarangMin <75628926+kgm7334@users.noreply.github.com>
2022-02-09 11:44:21 +0900

    강민커밋

--- PrismMvvmApp/PrismMvvmApp/ViewModels/BaseModelViewModel.cs
+++ PrismMvvmApp/PrismMvvmApp/ViewModels/BaseModelViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Prism.Events;
+﻿using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
 using PrismMvvmApp.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,14 +20,45 @@ namespace PrismMvvmApp.ViewModels
             set { SetProperty(ref _message, value); }
         }
 
+        private DelegateCommand<string> _baseModelGetPictureCommand;
+        public DelegateCommand<string> BaseModelGetPictureCommand
+        {
+            get 
+            {
+                return _baseModelGetPictureCommand;
+            }
+            set
+            {
+                SetProperty(ref _baseModelGetPictureCommand, value);
+            }
+        }
+        
+        /// <summary>
+        /// 파일 아이콘(비트맵)
+        /// </summary>
+        private string _baseModelPicture;
+        public string BaseModelPicture
+        {
+            get { return _baseModelPicture; }
+            set { SetProperty(ref _baseModelPicture, value); }
+        }
+
         public BaseModelViewModel(IEventAggregator eventAggregator)
         {
+
+            BaseModelGetPictureCommand = new DelegateCommand<string>(ChangeBaseModelPicture);
             Message = "This is BaseModel View";
             eventAggregator.GetEvent<MessageEvent>().Subscribe(MessageRecevied);
         }
 
-        private void MessageRecevied(string payload)
+        private void ChangeBaseModelPicture(string PictureName)
         {
+            string imagespath = @"C:\vms\Images\";
+            if (File.Exists(imagespath + PictureName))BaseModelPicture = imagespath + PictureName;
+        }
+
+        private void MessageRecevied(string payload)
+        { 
             Message = payload;
         }
     }
